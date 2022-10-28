18891d5cea7bdf4e849d1038eb38a5919253ee8d
josephlarrivy <joseph.larrivy@gmail.com>
2022-10-28 15:45:54 -0500

    registration and login implemented

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from forms import AddUserForm, UserForm
-from models import connect_db, db, User, List, ListTitle, ComicTitle
+from models import connect_db, db, User, List, ListComic, Comic
 from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
@@ -25,5 +25,48 @@ def redirect_home():
 
 @app.route('/home')
 def show_homepage():
-    user = User.query.get('user1')
-    return render_template('/main/home.html', user=user)
+    return render_template('/main/home.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_new_user():
+
+    form = AddUserForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        email = form.email.data
+
+        new_user = User.register_new_user(username, password, first_name, last_name, email)
+        db.session.add(new_user)
+
+        try:
+            db.session.commit()
+        except IntegrityError:
+            flash('username already taken')
+            return render_template('/users/register.html', form=form)
+
+        return redirect('/home')
+    
+    return render_template('/users/register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def log_in_user():
+
+    form = UserForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        user = User.authenticate(username,password)
+        if user:
+            session['username'] = user.username
+            return redirect('/')
+        else:
+            flash('Invalid username/password combination')
+            return render_template('/users/login.html', form=form)
+    
+    return render_template('/users/login.html', form=form)
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
+bcrypt = Bcrypt()
 db = SQLAlchemy()
 
 def connect_db(app):
@@ -22,7 +23,21 @@ class User(db.Model):
     last_name = db.Column(db.String(30), nullable=False)
     email = db.Column(db.String)
 
-    lists = db.relationship('Lists', backref='user')
+    # lists = db.relationship('Lists', backref='user')
+
+    @classmethod
+    def register_new_user(cls, username, password, first_name, last_name,email):
+        hashed = bcrypt.generate_password_hash(password)
+        hashed_utf8 = hashed.decode('utf8')
+        return cls(username=username, password=hashed_utf8, email=email, first_name=first_name, last_name=last_name)
+
+    @classmethod
+    def authenticate(cls, username, password):
+        u = User.query.filter_by(username=username).first()
+        if u and bcrypt.check_password_hash(u.password, password):
+            return u
+        else:
+            return False
 
 
 class List(db.Model):
